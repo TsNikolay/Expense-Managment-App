087764e12c2fd51c6b8aca1649c1b5e348eb67eb
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2024-01-01 22:28:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+// Файл конфігу dot env;
+dotenv.config();
+
+const app = express();
+
+//Мідлвари
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+//Маршрутизація
+app.get("/", (req, res) => {
+  res.send("<h1>Home page</h1>");
+});
+
+//Порт
+const PORT = 3000;
+
+//Прослуховування серверу
+app.listen(PORT, () => {
+  console.log(`Server running successfully on ${PORT} port`);
+});
