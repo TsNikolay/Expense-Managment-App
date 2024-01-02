7f74bd4faa39d03869d0f1a1b110103f6d9410a4
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2024-01-02 20:16:36 +0100

    React application was created for Client side

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">The client side is here</h1>
+    </div>
+  );
+}
+
+export default App;
