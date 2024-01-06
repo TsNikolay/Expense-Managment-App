1c05f0f2d5ac8ec0bc68da87ed7761de179897f9
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2024-01-06 03:35:29 +0100

    Minor changes for further deployment

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const dbConnect = require("./config/dbConnect");
+const path = require("path");
+
 // Файл конфігу dot env;
 dotenv.config();
 
@@ -19,6 +21,12 @@ app.use(cors());
 //Маршрутизація
 app.use("/api/v1/users", require("./routes/userRoute"));
 app.use("/api/v1/transactions", require("./routes/transactionRoute"));
+
+//Статичний файл
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //Порт
 const PORT = 8080 || process.env.PORT;
